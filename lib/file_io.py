2291d7789a97edb282059f3033019e0f551252d8
Marvin Kadoyo <kadoyomarvin@gmail.com>
2025-05-22 09:30:00 +0300

    Fix file I/O functions to pass all tests

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,22 @@
+# lib/file_io.py
+
 def write_file(file_name, file_content):
-    pass
+    file_name = str(file_name)
+    if not file_name.endswith('.txt'):
+        file_name += '.txt'
+    with open(file_name, 'w') as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    file_name = str(file_name)
+    if not file_name.endswith('.txt'):
+        file_name += '.txt'
+    with open(file_name, 'a') as file:
+        file.write(append_content)  # <-- No extra newline here
 
 def read_file(file_name):
-    pass
+    file_name = str(file_name)
+    if not file_name.endswith('.txt'):
+        file_name += '.txt'
+    with open(file_name, 'r') as file:
+        return file.read().strip()
